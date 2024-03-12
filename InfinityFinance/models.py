b62c7ac7782a506ac858880e16631745cb17a367
John Samuel <johnayobami77@proton.me>
2024-03-12 04:29:17 +0100

    Update models.py: Remove class Transaction and its imports.

--- InfinityFinance/models.py
+++ InfinityFinance/models.py
@@ -53,17 +53,6 @@ class Transaction(models.Model):
     def __str__(self):
         return self.description
 
-from django.db import models
-from .models import Account
-
-class Transaction(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.CharField(max_length=255)
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.description
 
 
 
